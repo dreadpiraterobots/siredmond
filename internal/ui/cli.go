f005ec06d1b2e29e7547b5ff8204bfe0f8ec598c
Adam <adam@dreadpiraterobots.ca>
2025-12-27 01:05:03 -0500

    CLI args/help initial

---
+++ internal/ui/cli.go
@@ -0,0 +1,28 @@
+package ui
+
+import (
+	"github.com/dreadpiraterobots/siredmond/internal/core"
+	"github.com/urfave/cli/v2"
+)
+
+func NewCLI(engine *core.Engine) *cli.App {
+	return &cli.App{
+		Name:  "siredmond",
+		Usage: "Microsoft security advisory analysis and exploration",
+		Commands: []*cli.Command{
+			{
+				Name:  "download",
+				Usage: "Download security advisory resources",
+				Subcommands: []*cli.Command{
+					{
+						Name:  "cvrf",
+						Usage: "Download CVRF data",
+						Action: func(c *cli.Context) error {
+							return engine.DownloadCVRF()
+						},
+					},
+				},
+			},
+		},
+	}
+}
