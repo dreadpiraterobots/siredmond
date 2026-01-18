d06bae76c69a59e13fd6ba35f9f1337e6b0d6c75
Adam <adam@dreadpiraterobots.ca>
2026-01-18 13:13:49 -0500

    Remove code which we already moved to download_cvrf.go

--- internal/core/download.go
+++ internal/core/download.go
@@ -2,7 +2,6 @@ package core
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"io"
 	"log/slog"
@@ -110,29 +109,4 @@ func (e *Engine) downloadAndCacheURL(ctx context.Context, url, cacheDir, filenam
 	}
 
 	return file, nil
-}
-
-// DownloadCVRF downloads the CVRF updates index, and then fetches individual CVRF documents
-// if the local cached copy is older/missing
-func (e *Engine) DownloadCVRF() error {
-	cacheDir, err := e.getCacheDir("cvrf", cvrfVersion)
-	if err != nil {
-		return err
-	}
-	e.logger.Debug("Updating CVRF index", "cvrfUpdatesURL", cvrfUpdatesURL, "cacheDir", cacheDir)
-
-	cvrfIndexUpdatesBody, err := e.downloadAndCacheURL(context.Background(), cvrfUpdatesURL, cacheDir, "updates.json")
-	if err != nil {
-		return err
-	}
-	defer cvrfIndexUpdatesBody.Close()
-
-	// Parse the JSON response of the updates index into the structs
-	var cvrfIndexUpdates CVRFIndexUpdates
-	if err := json.NewDecoder(cvrfIndexUpdatesBody).Decode(&cvrfIndexUpdates); err != nil {
-		return fmt.Errorf("failed to decode CVRF index updates: %w", err)
-	}
-	e.logger.Debug("CVRF index updates structs populated", "count", len(cvrfIndexUpdates.Value))
-
-	return nil
 }
