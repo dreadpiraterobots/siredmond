4d6f856ea782ee0fab916f356a2066e091bdfd79
Adam <adam@dreadpiraterobots.ca>
2026-01-18 13:51:59 -0500

    Refactor CVRF download

--- internal/core/download_cvrf.go
+++ internal/core/download_cvrf.go
@@ -13,19 +13,27 @@ func (e *Engine) DownloadCVRF() error {
 	if err != nil {
 		return err
 	}
+	cvrfIndex, err := e.DownloadCVRFIndex(cvrfCacheDir)
+	if err != nil {
+		return err
+	}
+	e.logger.Debug("CVRF index updates structs populated", "count", len(cvrfIndex.Value))
+
+	return nil
+}
+
+func (e *Engine) DownloadCVRFIndex(cvrfCacheDir string) (*CVRFIndexUpdates, error) {
 	e.logger.Debug("Updating CVRF index", "cvrfUpdatesURL", cvrfUpdatesURL, "cacheDir", cvrfCacheDir)
 	cvrfIndexUpdatesBody, err := e.downloadAndCacheURL(context.Background(), cvrfUpdatesURL, cvrfCacheDir, "updates.json")
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer cvrfIndexUpdatesBody.Close()
 
 	// Parse the JSON response of the updates index into the structs
 	var cvrfIndex CVRFIndexUpdates
 	if err := json.NewDecoder(cvrfIndexUpdatesBody).Decode(&cvrfIndex); err != nil {
-		return fmt.Errorf("failed to decode CVRF index updates: %w", err)
+		return nil, fmt.Errorf("failed to decode CVRF index updates: %w", err)
 	}
-	e.logger.Debug("CVRF index updates structs populated", "count", len(cvrfIndex.Value))
-
-	return nil
+	return &cvrfIndex, nil
 }
